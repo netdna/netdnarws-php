894b5781440c087a12f6719fccb60e352bc86df6
Karlo Espiritu <karlo.espiritu@gmail.com>
2012-09-22 16:11:27 +0800

    updated sample client to default params

--- sampleclient.php
+++ sampleclient.php
@@ -2,14 +2,6 @@
 
 require_once('NetDNA.php');
 
-$api = new NetDNA('kespiritu',"49949e1a0f27b17ab892e652363a70d705052af0f","6435b4a540b068a469fdae073ba2bc57");
+$api = new NetDNA("myalias", "consumer_key", "consumer_secret");
  
 echo  $api->get('/account.json');
-// 
-// zones/pull.json
-// 
-// require 'netdnarws'
-// 
-// api = NetDNARWS::NetDNA.new("myalias", "consumer_key", "consumer_secret")
-// 
-// api.get("/account.json")
